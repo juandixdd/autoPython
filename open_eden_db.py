42712c471baeeb75503c3a2c40d00e11c217ea06
JuanDi <juandixarteagaperez210@gmail.com>
2022-11-13 21:23:31 -0500

    Abrir proyecto del sena con python

---
+++ open_eden_db.py
@@ -0,0 +1,4 @@
+import os
+
+os.system('cmd /c "j: && cd J:\Education\Sena\EdenSoft\Front\eden_soft_angular_v5.0 && code . && cd .. && cd .. && cd Back && cd node_jsedenSoft_backEnd && code . && cd.. && cd.. && cd.. && cd.. && cd.. && d: && cd D:\Programs\DBeaver && start dbeaver.exe"')
+# C for close cmd and k for not close cmd
